bfd53c223ce03aad5c79836e7b63b72ea3dc0e84
acasia <mi@naver.com>
2017-09-07 11:09:47 +0900

    Review

--- 20171604_KimJinwoo_assignment2.py
+++ 20171604_KimJinwoo_assignment2.py
@@ -8,16 +8,13 @@ Assignment2
 try:
     n = int(input("Enter a number: "))
     sum = 1
-    while True:
-        if n == -1:
-            break
-        elif n < -1:
-            print("Error")
-            break
+    while n >= 0:
         for i in range(1, n + 1):
-            sum = sum * i
+            sum *= i
         print("%d!" % n, " = ", sum)
         sum = 1
         n = int(input("Enter a number: "))
+    if n <= -1:
+        print("Error")
 except ValueError:
     print("Error")
