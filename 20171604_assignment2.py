00b2d8bfaa359126d5cc07581f9c72b95dafa35c
acasia <mi@naver.com>
2017-09-07 10:54:10 +0900

    KimJinWoo

---
+++ 20171604_assignment2.py
@@ -0,0 +1,23 @@
+"""
+KMUSW
+Software Project2
+Assignment2
+20171604 KimJinwoo
+"""
+
+try:
+    n = int(input("Enter a number: "))
+    sum = 1
+    while True:
+        if n == -1:
+            break
+        elif n < -1:
+            print("Error")
+            break
+        for i in range(1, n + 1):
+            sum = sum * i
+        print("%d!" % n, " = ", sum)
+        sum = 1
+        n = int(input("Enter a number: "))
+except ValueError:
+    print("Error")
