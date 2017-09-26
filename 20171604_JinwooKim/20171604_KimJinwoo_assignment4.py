d84827f7cde005cc7645b402ca387aec3806310a
acasia <mi@naver.com>
2017-09-26 11:07:43 +0900

    Code Review2

--- 20171604_JinwooKim/20171604_KimJinwoo_assignment4.py
+++ 20171604_JinwooKim/20171604_KimJinwoo_assignment4.py
@@ -8,13 +8,16 @@ Combination.
 """
 
 def fac(a):
-    if a == 1:
+    if a == 1 or a == 0:
         return 1
     else:
         return a * fac(a-1)
 
 n = int(input("Enter n: "))
 m = int(input("Enter m: "))
+
+
+
 try:
     if n==0 and n < m:
         result = 1
@@ -23,5 +26,8 @@ try:
         exit()
     result = fac(n) / (fac(m)*fac(n-m))
 except RecursionError:
-    result = 1
+    print("RecursionError")
+    exit()
+
+result = fac(n) / (fac(m)*fac(n-m))
 print("C(%d,%d) = %d "%(n,m,result))
