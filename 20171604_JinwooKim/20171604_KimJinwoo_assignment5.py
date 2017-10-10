f6191732064caf46525fbdd150131bf3d29b669c
acasia <mi@naver.com>
2017-10-10 11:02:28 +0900

    code review

--- 20171604_JinwooKim/20171604_KimJinwoo_assignment5.py
+++ 20171604_JinwooKim/20171604_KimJinwoo_assignment5.py
@@ -1,14 +1,37 @@
 import time
+
+def iterfibo(a):
+    fn = 0
+    sn = 1
+    result = 0
+    for i in range(a):
+        fn = sn
+        sn = result
+        result = fn + sn
+    return result
+
+def fibo(s):
+    try :
+        if s <=1:
+            return s
+        return fibo(s-1) + fibo(s-2)
+    except ValueError:
+        print("Error")
+        exit()
+
 n = int(input("N : "))
-fn = 0
-sn = 1
-result = 0
+fibo_result = 0
+iterfibo_result = 0
+
 if n == -1:
     exit()
-ts =time.time()
-for i in range(n):
-    fn = sn
-    sn = result
-    result = fn + sn
-ts = time.time()- ts
-print(result,ts)
+
+ts = time.time()
+fibo_result = fibo(n)
+ts = time.time() - ts
+print("fibo time : %.6f, Value : %d" %(ts, fibo_result))
+
+ts = time.time()
+iterfibo_result = iterfibo(n)
+ts = time.time() - ts
+print("iterfibo time : %.6f, Value : %d" %(ts, iterfibo_result))
